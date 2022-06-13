a8a82e641f3a7d295552dd66a238daef08d446a0
Habis <habis.ahmad123@gmail.com>
2022-06-12 20:41:30 -0400

    Exercise 2 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
 const repeatString = function() {
-
+    let a = 'hey'
+    for(let i = 0; i < 3; i++){
+        let c = a + a + a
+        if(i == 2){
+            return c
+        }
+    }
+    
 };
 
 // Do not edit below this line
