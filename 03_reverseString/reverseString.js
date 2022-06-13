d5312b1ba7aec66ff6f6a0d97ab50fb6a23373ab
Habis <habis.ahmad123@gmail.com>
2022-06-12 21:36:07 -0400

    Exercise 3 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
 const reverseString = function() {
+    let a = "hello"
+    let array = []
+    for(let i = 0; i < a.length; i++){
+        array[i] = a.charAt(i);
+    }
+    arrayReverse = []
+    for(let i = 0; i < a.length; i++){
+        arrayReverse[i] = array.pop()
+    }
 
-};
+    return arrayReverse.join("")
+}
 
 // Do not edit below this line
 module.exports = reverseString;
