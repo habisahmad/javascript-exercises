7a0b0ba2c566918a2c64eff45cc7602b13c9ba04
Habis <habis.ahmad123@gmail.com>
2022-06-15 18:43:34 -0400

    Exercise 5 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
 const sumAll = function() {
-
+    let x = 1
+    let y = 4
+    let yans = y 
+    let xans = x
+    x = x + y
+    for(let i = xans; i < yans; i++){
+        y = y - 1
+        x = x + y
+    }
+    return x - 1
+    
 };
 
 // Do not edit below this line
