129f0b0e9aa74afc5e0662b9d1f2e652344e8c87
Habis <habis.ahmad123@gmail.com>
2022-06-15 18:51:32 -0400

    Exercise 6 completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,10 @@
 const leapYears = function() {
-
+    year = 1996;
+    if(year % 4 == 0 && year % 100 != 0 || year % 100 == 0 && year % 400 == 0){
+        return true
+    }else if(year % 4 != 0 && year % 100 != 0 && year % 400 != 0){
+        return false
+    }
 };
 
 // Do not edit below this line
